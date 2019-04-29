9ccf901ac88c3b8dd9c490d12d821a3262535d46
Jessica Davey <jessica@jessicadavey.com>
2019-04-29 18:16:45 -0500

    added a reset button

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const counter = document.querySelector(".counter");
 const undo = document.querySelector(".undo");
+const reset = document.querySelector(".reset");
 
 let count = localStorage.getItem("count") || 0;
 counter.textContent = count;
 
 counter.addEventListener("click", handleIncrement);
-
 undo.addEventListener("click", handleUndo);
+reset.addEventListener("click", handleReset);
 
 function handleIncrement() {
   if (count >= 16) return;
@@ -18,6 +19,12 @@ function handleIncrement() {
 function handleUndo() {
   if (count <= 0) return;
   count--;
+  localStorage.setItem("count", count);
+  counter.textContent = count;
+}
+
+function handleReset() {
+  count = 0;
   localStorage.setItem("count", count);
   counter.textContent = count;
 }
